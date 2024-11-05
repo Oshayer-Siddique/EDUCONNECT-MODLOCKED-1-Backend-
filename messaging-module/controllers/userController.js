8c51d3b94eaf297d70ce9913c410c2d9257db72b
Yead Khail <119938455+yeadkhail@users.noreply.github.com>
2024-11-05 21:37:59 +0600

    Added messaging-module and created basic structure and files

---
+++ messaging-module/controllers/userController.js
@@ -0,0 +1,20 @@
+const registerLoad = async(req,res)=>{
+    try{
+        res.render('register');
+    }catch(error){
+        console.log(error.message);
+    }
+}
+
+const register = async(req,res)=>{
+    try{
+
+    }catch(error){
+        console.log(error.message);
+    }
+}
+
+module.exports = {
+    registerLoad,
+    register
+}
