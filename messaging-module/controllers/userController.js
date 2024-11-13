57ea848a64767feb57df6f0e4c49e8d4f9a3b9ab
Yead <119938455+yeadkhail@users.noreply.github.com>
2024-11-13 23:34:56 +0600

    Did some progress on the chatting. ajax not working error is there

--- messaging-module/controllers/userController.js
+++ messaging-module/controllers/userController.js
@@ -1,5 +1,7 @@
 const User = require('../models/userModel');
+const Chat = require('../models/chatModel');
 const bcrypt = require('bcrypt');
+const { response } = require('../routes/userRoute');
 
 
 const registerLoad = async(req,res)=>{
@@ -75,11 +77,26 @@ const loadDashboard = async(req, res) => {
     }
 } 
 
+const saveChat = async(req,res)=>{
+    try{
+        var chat = new Chat({
+            sender_id:req.body.sender_id,
+            receiver_id:req.body.receiver_id,
+            message:req.body.message,
+        });
+        var newChat = await chat.save();
+        res.status(200).send({success:true, msg:'chat inserted!',data:newChat});
+    }catch(error){
+        res.status(400).send({success:false, msg:error.message});
+    }
+}
+
 module.exports = {
     registerLoad,
     register,
     loadLogin,
     logout,
     login,
-    loadDashboard
+    loadDashboard,
+    saveChat
 }
