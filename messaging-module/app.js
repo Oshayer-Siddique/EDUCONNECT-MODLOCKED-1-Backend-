76e7037e0f1ec9cf53c4b7862bee9093e7a58bc7
Yead <119938455+yeadkhail@users.noreply.github.com>
2024-11-16 20:52:17 +0600

    Completed the chatting between two users and now we can load old chats

--- messaging-module/app.js
+++ messaging-module/app.js
@@ -23,6 +23,8 @@ const userRoute = require('./routes/userRoute');
 
 const User = require('./models/userModel');
 
+const Chat = require('./models/chatModel');
+
 app.use('/',userRoute);
 
 const io = require('socket.io')(http);
@@ -55,6 +57,19 @@ usp.on('connection',async function(socket){
       //chatting implementation
       socket.on('newChat',function(data){
         socket.broadcast.emit('loadNewChat',data);
+      });
+
+      //load old chats
+      socket.on('existsChat',async function(data){
+        var chats = await Chat.find({ $or: [
+            {
+              sender_id: data.sender_id, receiver_id: data.receiver_id
+            },
+            {
+              sender_id: data.receiver_id, receiver_id: data.sender_id
+            },
+        ]});
+        socket.emit('loadChats',{ chats: chats});
       });
 });
 
